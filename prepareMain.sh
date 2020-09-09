05fa38f26f747b62674923b64e82e71f23201b06
nathPay <45284476+nathPay@users.noreply.github.com>
2020-09-09 15:23:14 +0200

    Delete prepareMain.sh

--- prepareMain.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scp -r ./server/repos/poa-deployment-bench/ npayre@frontend.lyon.grid5000.fr:. < "/dev/null"
-scp -r ./server/scenarios/ npayre@frontend.lyon.grid5000.fr:. < "/dev/null"
-scp -r ./server/scripts/ npayre@frontend.lyon.grid5000.fr:. < "/dev/null"
